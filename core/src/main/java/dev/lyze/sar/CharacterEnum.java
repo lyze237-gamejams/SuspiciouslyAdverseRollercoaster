101763a6cf6098feabab5f83ca586503a35e4c9b
Lyze <11274700+lyze237@users.noreply.github.com>
2022-03-28 21:31:19 +0200

    Renamed fox to foxy

--- core/src/main/java/dev/lyze/sar/CharacterEnum.java
+++ core/src/main/java/dev/lyze/sar/CharacterEnum.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum CharacterEnum {
-    FOX("Fox", "Landing", 1, false),
+    FOXY("Fox", "Landing", 1, false),
     //ZEALOT("Zealot", "Landing", 1, false),
     LYZE("Lyze", "CrouchIn", 3, true);
 
